d593c3ec59d8e568b6e8d4fa2137d80c2db2b660
Jack Cook <hello@jackcook.nyc>
2015-09-05 00:50:26 -0400

    Retrieve data from language files

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(function() {
   ];
 
   var languageTags = [
+    "English",
     "French",
     "Spanish",
     "Swedish"
@@ -19,7 +20,38 @@ $(function() {
   });
 });
 
+var actions = {
+  "read a book": "book",
+  "watch a movie": "movie",
+  "read the news": "news"
+}
+
+var languages = {
+  "english": "en",
+  "spanish": "es",
+  "french": "fr",
+  "swedish": "sv"
+}
+
+function get_links(action, language) {
+  file = "languages/" + languages[language.toLowerCase()] + ".json";
+
+  request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 3) {
+      var response = JSON.parse(request.responseText)[actions[action]];
+      console.log(response);
+    }
+  }
+
+  request.open("GET", file, true);
+  request.send();
+}
+
 var go = document.getElementById("go");
+var actionsElement = document.getElementById("actions");
+var languagesElement = document.getElementById("languages");
+
 go.addEventListener("click", function() {
-  console.log("yey");
+  get_links(actionsElement.value, languagesElement.value);
 });
