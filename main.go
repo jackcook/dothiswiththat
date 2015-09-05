457eb6f97f9aa3eb2eb37158f1404c241a72b1fc
Jack Cook <hello@jackcook.nyc>
2015-09-04 22:33:49 -0400

    Implement autocomplete

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-  "fmt"
+  "html/template"
+  "io/ioutil"
   "net/http"
 )
 
+type Page struct {
+    Title string
+    Body  []byte
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Testing")
+  // p := loadPage(title)
+  p := &Page{Title: "test"}
+  t, _ := template.ParseFiles("index.html")
+  t.Execute(w, p)
+}
+
+func loadPage(title string) *Page {
+    filename := title + ".txt"
+    body, _ := ioutil.ReadFile(filename)
+    return &Page{Title: title, Body: body}
 }
 
 func main() {
