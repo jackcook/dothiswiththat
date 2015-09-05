badc8d799e194262801b4b6e1ca1ed8f0693af9c
Jack Cook <hello@jackcook.nyc>
2015-09-05 17:26:05 -0400

    Fixed go file

--- main.go
+++ main.go
@@ -1,30 +1,13 @@
 package main
 
 import (
-  "html/template"
-  "io/ioutil"
   "net/http"
 )
 
-type Page struct {
-    Title string
-    Body  []byte
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-  // p := loadPage(title)
-  p := &Page{Title: "test"}
-  t, _ := template.ParseFiles("index.html")
-  t.Execute(w, p)
-}
-
-func loadPage(title string) *Page {
-    filename := title + ".txt"
-    body, _ := ioutil.ReadFile(filename)
-    return &Page{Title: title, Body: body}
-}
-
 func main() {
-  http.HandleFunc("/", handler)
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, r.URL.Path[1:])
+  })
+
   http.ListenAndServe(":8080", nil)
 }
